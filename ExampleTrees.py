08f6533e53560f04d3217bcf4a88c4859b89f0b0
Chris Tralie <chris.tralie@gmail.com>
2016-12-04 17:46:28 -0500

    Tree subdivision now implemented

--- ExampleTrees.py
+++ ExampleTrees.py
@@ -4,33 +4,45 @@ import matplotlib.pyplot as plt
 
 def getTreeA():
     T = MergeTree(TotalOrder2DX)
-    T.root = MergeNode(8, np.array([0, 8]))
-    A = MergeNode(5, np.array([0, 5]))
+    T.root = MergeNode(np.array([0, 8]))
+    A = MergeNode(np.array([0, 5]))
     T.root.addChild(A)
-    B = MergeNode(0, np.array([-3, 0]))
-    C = MergeNode(4, np.array([1, 4]))
+    B = MergeNode(np.array([-3, 0]))
+    C = MergeNode(np.array([1, 4]))
     A.addChildren([B, C])
-    D = MergeNode(3.5, np.array([0.5, 3.5]))
-    E = MergeNode(2, np.array([2, 2]))
+    D = MergeNode(np.array([0.5, 3.5]))
+    E = MergeNode(np.array([2, 2]))
     C.addChildren([D, E])
     return T
 
 def getTreeB():
     T = MergeTree(TotalOrder2DX)
-    T.root = MergeNode(8, np.array([0, 8]))
-    A = MergeNode(5, np.array([0, 5]))
+    T.root = MergeNode(np.array([0, 8]))
+    A = MergeNode(np.array([0, 5]))
     T.root.addChild(A)
-    B = MergeNode(4, np.array([-1, 4]))
-    C = MergeNode(2, np.array([2, 2]))
+    B = MergeNode(np.array([-1, 4]))
+    C = MergeNode(np.array([2, 2]))
     A.addChildren([B, C])
-    D = MergeNode(0, np.array([-2, 0]))
-    E = MergeNode(3.5, np.array([-0.5, 3.5]))
+    D = MergeNode(np.array([-2, 0]))
+    E = MergeNode(np.array([-0.5, 3.5]))
     B.addChildren([D, E])
     return T
 
 if __name__ == "__main__":
     TA = getTreeA()
-    TA.render(np.array([0, 0]))
+    #TA.addOffset(np.array([0, 0.3]))
     TB = getTreeB()
+
+    valsA = TA.getfValsSorted()
+    valsB = TB.getfValsSorted()
+
+    TB.subdivideFromValues(valsB)
+    TA.subdivideFromValues(valsA)
+
+    fig = plt.figure()
+    TA.render(np.array([0, 0]))
     TB.render(np.array([6, 0]))
+    ax = fig.gca()
+    ax.set_yticks(np.arange(-1, 9, 0.5))
+    plt.grid()
     plt.show()
--- MergeTree.py
+++ MergeTree.py
@@ -1,5 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from bisect import bisect_left
+
+
 
 #For trees embedded in the plane corresponding to
 #1D functions, make a total order based on the X
@@ -22,13 +25,17 @@ def PartialOrder3DXY(N1, N2):
     return 0
 
 class MergeNode(object):
-    #fVal: Function Value
-    #X: Rendering position (not used in actual merge tree comparisons but used to help partial order)
-    def __init__(self, fVal, X):
+    #X: Rendering position.  Last coordinate is assumed to be the function value
+    #to be the function value
+    #newNode: Whether this node was added in a subdivision (for debugging)
+    def __init__(self, X, newNode = False):
         self.parent = None
         self.children = []
-        self.fVal = fVal #Function value
-        self.X = X
+        self.X = np.array(X, dtype=np.float64)
+        self.newNode = newNode
+
+    def getfVal(self):
+        return self.X[-1]
 
     def addChild(self, N):
         self.children.append(N)
@@ -43,10 +50,23 @@ class MergeTree(object):
     def __init__(self, orderFn):
         self.root = None
         self.orderFn = orderFn
+        self.fVals = []
+
+    def addOffsetRec(self, node, offset):
+        node.X += offset
+        for C in node.children:
+            self.addOffsetRec(C, offset)
+
+    def addOffset(self, offset):
+        self.addOffsetRec(self.root, offset)
 
     def renderRec(self, node, offset):
         X = node.X + offset
-        plt.scatter(X[0], X[1], 40, 'r')
+        if node.newNode:
+            #Render new nodes blue
+            plt.scatter(X[0], X[1], 40, 'b')
+        else:
+            plt.scatter(X[0], X[1], 40, 'r')
         if node.parent:
             Y = node.parent.X + offset
             plt.plot([X[0], Y[0]], [X[1], Y[1]], 'k')
@@ -54,8 +74,60 @@ class MergeTree(object):
             self.renderRec(C, offset)
 
     def render(self, offset):
+        plt.hold(True)
         self.renderRec(self.root, offset)
-        plt.grid()
 
-    def subdivideFromOtherTree(self, T):
-        print "TODO"
+    def getfValsSortedRec(self, node):
+        self.fVals.append(node.getfVal())
+        for n in node.children:
+            self.getfValsSortedRec(n)
+
+    #Get a sorted list of all of the function values
+    def getfValsSorted(self):
+        self.fVals = []
+        self.getfValsSortedRec(self.root)
+        self.fVals = sorted(self.fVals)
+        self.fVals = np.unique(self.fVals)
+        return self.fVals
+
+    #hi: Index such that vals[0:hi] < N1.fVal
+    def subdivideEdgesRec(self, N1, vals, hi):
+        b = N1.getfVal()
+        if b <= vals[0]:
+            return
+        for i in range(len(N1.children)):
+            N2 = N1.children[i]
+            a = N2.getfVal()
+            #Figure out the elements in vals which
+            #are in the open interval (a, b)
+            lo = bisect_left(vals, a)
+            splitVals = []
+            for k in range(lo, hi+1):
+                if vals[k] <= a or vals[k] >= b:
+                    continue
+                splitVals.append(vals[k])
+            if len(splitVals) > 0:
+                #Now split the edge between N1 and N2
+                newNodes = []
+                for k in range(len(splitVals)):
+                    t = (splitVals[k] - a)/float(b - a)
+                    X = t*N1.X + (1-t)*N2.X
+                    N = MergeNode(X, True)
+                    if k > 0:
+                        newNodes[k-1].addChild(N)
+                    newNodes.append(N)
+                #The last node is connected to N2
+                newNodes[-1].addChild(N2)
+                #Replace N2 in N1's children list with
+                #the first node in newNodes
+                N1.children[i] = newNodes[0]
+                newNodes[0].parent = N1
+            self.subdivideEdgesRec(N2, vals, lo)
+
+
+    #Note: For simplicity of implementation, this
+    #function assumes that parent nodes have higher
+    #function values than their children
+    def subdivideFromValues(self, vals):
+        hi = bisect_left(vals, self.root.getfVal())
+        self.subdivideEdgesRec(self.root, vals, hi)
